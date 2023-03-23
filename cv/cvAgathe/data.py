96c99f5ebff31bc6428b5797506133155718bcac
AgatheBecquart <inviteagathe@gmail.com>
2023-03-23 09:19:22 +0100

    cv terminé

---
+++ cv/cvAgathe/data.py
@@ -0,0 +1,128 @@
+# type here title you want
+titleCV = "Django Personal CV"
+
+# type here your name and surname
+yourName = "Agathe Becquart"
+
+# type here current profession
+yourProfession = "En formation dans la Data."
+
+# type here your bio
+yourBio = "Je suis actuellement en recherche d'une alternance dans la Data/IA"
+
+# type here your current location
+yourCountry = "Lille/France"
+
+# if you want add another social media account, add here icon code of it
+socialContact = [
+    'fab fa-github',
+    'fab fa-linkedin',
+    'fab fa-instagram',
+    'fab fa-twitter',
+    'fab fa-facebook',
+    'fas fa-envelope',
+    'fas fa-phone-alt'
+]
+
+# type here some contact
+yourContact = [
+    # github link
+    'https://github.com',
+    # linkedin link
+    'https://linkedin.com',
+    # instagram link
+    'https://instagram.com',
+    # twitter link
+    'https://twitter.com',
+    # facebook link
+    'https://facebook.com',
+    # mailto:...your email
+    'mailto:asd@domain.com',
+    # tel:...your phone no
+    'tel:05554446699'
+]
+
+# type here your birthday and (your age)
+yourBirthday = "19 Fev 1998 (25)"
+
+# type here your skills
+yourSkills = [ 
+    'Python',
+    'HTML/CSS',
+    'Pandas',
+    'Django',
+    'Matplotlib, Seaborn',
+    'MySQL'
+]
+
+# type here your skills percent rate
+skillsPercent = [
+    '65',
+    '60',
+    '60',
+    '55',
+    '40',
+    '05',
+]
+
+# type here your hobbies
+yourHobbies = [
+    'Chant',
+    'Théâtre',
+    'Danse'
+]
+
+# type here your certificates
+yourCerts = [
+    'Autonomie',
+    'Curiosité',
+    'Rigueur',
+    'Respect',
+    'Ecoute',
+    'Détermination'
+]
+
+# type here your education info
+yourEdu = [
+    'Baccalauréat Scientifique - Lycée Saint Paul à Lille',
+    'Bachelor Arts Culinaires et entrepreunariat option Pâtisserie - Ferrandi Paris'
+]
+
+# type here your education years (start-end)
+eduYear = [
+    '2016',
+    '2019'
+]
+
+# type here your work history
+yourWork = [
+    'Developpeur en Intelligence Artificielle - Ecole IA Microsoft by Simplon.co'
+]
+
+# type here your work years (start-end)
+workYear = [
+    'Mars 2023 - En cours'
+]
+
+# type here your projects
+yourProject = [
+    'Demi-Chef de Partie Pâtisserie',
+    'Assistante de Professeur de Français'
+]
+
+# type here your projects link (if there is)
+projectLink = [
+    'Sept 2019 - Oct 2021',
+    'Oct 2021 - Mars 2022'
+]
+
+# type here your extras (driving license etc.)
+yourExtras = [
+    'Anglais B1',
+    'Espagnol C1',
+]
+
+# footer text in here (please don't change)
+footerText = "Django Personal CV developed by <a target='_blank' class='author' href='https://github.com/en0ndev'>en0ndev</a>"
+
+# thank you for use this template.
